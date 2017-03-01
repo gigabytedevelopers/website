733c7d9f343a83077173121e21d460fe5b1b9864
Gigabyte Developers Incorporated <gigabytedevelopers@gmail.com>
2017-03-01 02:42:31 +0100

    Add files via upload

---
+++ includes/overall/overallfooter.php
@@ -0,0 +1,4 @@
+</div>
+<?php include 'includes/footer.php'; ?>
+</body>
+</html>
---
+++ includes/overall/overallheaders.php
@@ -0,0 +1,6 @@
+<!doctype html>
+<html>
+<?php include 'includes/head.php'; ?>
+<body>
+	<div class="container">
+<?php include 'includes/header.php'; ?>
