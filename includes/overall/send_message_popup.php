f5bb68a13021dedcddf3e93f4cf639c7b51f5e1c
Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>
2020-05-23 16:09:43 +0100

    User feedback form
    Signed-off-by: Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>

---
+++ includes/overall/send_message_popup.php
@@ -0,0 +1,41 @@
+<div class="window-popup message-popup">
+    <a href="#" class="popup-close js-popup-close cd-nav-trigger">
+        <svg class="utouch-icon utouch-icon-cancel-1"><use xlink:href="#utouch-icon-cancel-1"></use></svg>
+    </a>
+
+    <div class="send-message-popup">
+        <h5>Send a Message</h5>
+        <p>Gigabyte Developers General Public Feedback.</p>
+        <form class="contact-form" method="post" action="send_mail.php">
+            <div class="with-icon">
+                <input name="name" placeholder="Your Name" type="text" required="required">
+                <svg class="utouch-icon utouch-icon-user"><use xlink:href="#utouch-icon-user"></use></svg>
+            </div>
+
+            <div class="with-icon">
+                <input name="email" placeholder="Email Address" type="text" required="required">
+                <svg class="utouch-icon utouch-icon-message-closed-envelope-1"><use xlink:href="#utouch-icon-message-closed-envelope-1"></use></svg>
+            </div>
+
+            <div class="with-icon">
+                <input class="with-icon" name="phone" placeholder="Phone Number" type="tel" required="required">
+                <svg class="utouch-icon utouch-icon-telephone-keypad-with-ten-keys"><use xlink:href="#utouch-icon-telephone-keypad-with-ten-keys"></use></svg>
+            </div>
+
+            <div class="with-icon">
+                <input class="with-icon" name="subject" placeholder="Subject" type="text" required="required">
+                <svg class="utouch-icon utouch-icon-icon-1"><use xlink:href="#utouch-icon-icon-1"></use></svg>
+            </div>
+
+            <div class="with-icon">
+                <textarea name="message" required="" placeholder="Your Feedback to Us" style="height: 180px;"></textarea>
+                <svg class="utouch-icon utouch-icon-edit"><use xlink:href="#utouch-icon-edit"></use></svg>
+            </div>
+
+            <button class="btn btn--green btn--with-shadow full-width">
+                Send Us your Feedback
+            </button>
+
+        </form>
+    </div>
+</div>
