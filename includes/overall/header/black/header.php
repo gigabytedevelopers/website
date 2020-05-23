e499ed98e47b3a76d095bcaeab5a5a8bd947c85e
Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>
2020-05-23 15:18:18 +0100

    Added overall dark header
    Signed-off-by: Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>

---
+++ includes/overall/header/black/header.php
@@ -0,0 +1,209 @@
+<header class="header header--menu-rounded header--dark" id="site-header">
+
+    <div class="header-lines-decoration">
+        <span class="bg-secondary-color"></span>
+        <span class="bg-blue"></span>
+        <span class="bg-blue-light"></span>
+        <span class="bg-orange-light"></span>
+        <span class="bg-red"></span>
+        <span class="bg-green"></span>
+        <span class="bg-secondary-color"></span>
+    </div>
+
+    <div class="container">
+
+        <a href="#" id="top-bar-js" class="top-bar-link"><svg class="utouch-icon utouch-icon-arrow-top"><use xlink:href="#utouch-icon-arrow-top"></use></svg></a>
+
+        <div class="header-content-wrapper">
+
+            <div class="site-logo">
+                <a href="index" class="full-block"></a>
+                <img src="img/banner_white.png" alt="Gigabyte Developers">
+            </div>
+
+            <nav id="primary-menu" class="primary-menu">
+
+                <!-- menu-icon-wrapper -->
+
+                <a href='javascript:void(0)' id="menu-icon-trigger" class="menu-icon-trigger showhide">
+                    <span class="mob-menu--title">Menu</span>
+                    <span id="menu-icon-wrapper" class="menu-icon-wrapper">
+						<svg width="1000px" height="1000px">
+							<path id="pathD" d="M 300 400 L 700 400 C 900 400 900 750 600 850 A 400 400 0 0 1 200 200 L 800 800"></path>
+							<path id="pathE" d="M 300 500 L 700 500"></path>
+							<path id="pathF" d="M 700 600 L 300 600 C 100 600 100 200 400 150 A 400 380 0 1 1 200 800 L 800 200"></path>
+						</svg>
+					</span>
+                </a>
+
+                <ul class="primary-menu-menu">
+                    <li class="menu-item-has-children">
+                        <a href="index">Home</a>
+                    </li>
+
+                    <li class="">
+                        <a href="#">Company</a>
+                        <ul class="sub-menu">
+                            <li>
+                                <a href="about">
+                                    About Us
+                                </a>
+                            </li>
+<!--                            <li class="menu-item-has-children">-->
+<!--                                <a href="#">-->
+<!--                                    Services-->
+<!--                                </a>-->
+<!--                                <ul class="sub-menu">-->
+<!--                                    <li>-->
+<!--                                        <a href="05_case_details_ver_01.html">Software Development</a>-->
+<!--                                    </li>-->
+<!---->
+<!--                                    <li>-->
+<!--                                        <a href="06_case_details_ver_02.html">Web Development</a>-->
+<!--                                    </li>-->
+<!---->
+<!--                                    <li>-->
+<!--                                        <a href="07_case_details_ver_03.html">Internet Marketing</a>-->
+<!--                                    </li>-->
+<!--                                </ul>-->
+<!--                            </li>-->
+                            <li>
+                                <a href="products">
+                                    Products
+                                </a>
+                            </li>
+                            <li>
+                                <a href="events_genesys_season_2.php">
+                                    Events
+                                </a>
+                            </li>
+                            <li class="">
+                                <a href="javascript:void(0)">
+                                    Tutorials
+                                </a>
+                            </li>
+                            <li>
+                                <a href="services">
+                                    Our Services
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="32_testimonials.html">
+                                    Testimonials
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li class="menu-item-has-mega-menu menu-item-has-children">
+                        <a href="#">Products</a>
+                        <div class="megamenu with-products">
+                            <div class="megamenu-row">
+
+                                <div class="col4" data-mh="product-item">
+                                    <div class="product-item">
+                                        <div class="product-item-thumb">
+                                            <div class="square-colored bg-product-blue"></div>
+                                            <img src="img/product-item1.jpg" alt="product">
+                                        </div>
+                                        <div class="product-item-content">
+                                            <h6 class="title">Splanner<br>Handy Student Planner</h6>
+                                            <a href="05_case_details_ver_01.html" class="more-arrow">
+                                                <span>View Splanner</span>
+                                                <div class="btn-next">
+                                                    <svg class="utouch-icon icon-hover utouch-icon-arrow-right-1"><use xlink:href="#utouch-icon-arrow-right-1"></use></svg>
+                                                    <svg class="utouch-icon utouch-icon-arrow-right1"><use xlink:href="#utouch-icon-arrow-right1"></use></svg>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col4" data-mh="product-item">
+                                    <div class="product-item">
+                                        <div class="product-item-thumb">
+                                            <div class="square-colored bg-product-violet"></div>
+                                            <img src="img/product-item2.jpg" alt="product">
+                                        </div>
+                                        <div class="product-item-content">
+                                            <h6 class="title">FireFiles<br>Powerful File Manager</h6>
+                                            <a href="06_case_details_ver_02.html" class="more-arrow">
+                                                <span>View FireFiles</span>
+                                                <div class="btn-next">
+                                                    <svg class="utouch-icon icon-hover utouch-icon-arrow-right-1"><use xlink:href="#utouch-icon-arrow-right-1"></use></svg>
+                                                    <svg class="utouch-icon utouch-icon-arrow-right1"><use xlink:href="#utouch-icon-arrow-right1"></use></svg>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col4" data-mh="product-item">
+                                    <div class="product-item">
+                                        <div class="product-item-thumb">
+                                            <div class="square-colored bg-product-red"></div>
+                                            <img src="img/product-item3.jpg" alt="product">
+                                        </div>
+                                        <div class="product-item-content">
+                                            <h6 class="title">CometOTP<br>2-Factor Authenticator</h6>
+                                            <a href="07_case_details_ver_03.html" class="more-arrow">
+                                                <span>View CometOTP</span>
+                                                <div class="btn-next">
+                                                    <svg class="utouch-icon icon-hover utouch-icon-arrow-right-1"><use xlink:href="#utouch-icon-arrow-right-1"></use></svg>
+                                                    <svg class="utouch-icon utouch-icon-arrow-right1"><use xlink:href="#utouch-icon-arrow-right1"></use></svg>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col4" data-mh="product-item">
+                                    <div class="product-item">
+                                        <div class="product-item-thumb">
+                                            <div class="square-colored bg-product-blue-dark"></div>
+                                            <img src="img/product-item4.jpg" alt="product">
+                                        </div>
+                                        <div class="product-item-content">
+                                            <h6 class="title">CalcMate<br>Powerful Calculator</h6>
+                                            <a href="05_case_details_ver_01.html" class="more-arrow">
+                                                <span>View CalcMate</span>
+                                                <div class="btn-next">
+                                                    <svg class="utouch-icon icon-hover utouch-icon-arrow-right-1"><use xlink:href="#utouch-icon-arrow-right-1"></use></svg>
+                                                    <svg class="utouch-icon utouch-icon-arrow-right1"><use xlink:href="#utouch-icon-arrow-right1"></use></svg>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </li>
+
+                    <li class="menu-item-has-mega-menu menu-item-has-children">
+                        <a href="#">Events</a>
+                    </li>
+
+                    <li class="">
+                        <a href="pricing">Pricing</a>
+                    </li>
+
+                    <li class="">
+                        <a href="contact">Contact</a>
+                    </li>
+                </ul>
+                <ul class="nav-add">
+                    <li class="search search_main">
+                        <a href="#" class="js-open-search-popup">
+                            <svg class="utouch-icon utouch-icon-search cd-nav-trigger"><use xlink:href="#utouch-icon-search"></use></svg>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+
+        </div>
+
+    </div>
+
+</header>
