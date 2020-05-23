ebb1d7e35ed234fe8a2f9292b5d3a924c950c119
Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>
2020-05-23 16:10:44 +0100

    Added search pop-up (not complete)
    Signed-off-by: Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>

---
+++ includes/overall/search_popup.php
@@ -0,0 +1,31 @@
+<div class="search-popup search--dark">
+    <a href="#" class="popup-close js-popup-close cd-nav-trigger">
+        <svg class="utouch-icon utouch-icon-cancel-1"><use xlink:href="#utouch-icon-cancel-1"></use></svg>
+    </a>
+
+    <div class="search-full-screen">
+
+        <div class="search-standard">
+
+            <form id="search-header-1" name="form-search-header" method="post">
+                <div class="typeahead__container">
+                    <div class="typeahead__field">
+
+						<span class="typeahead__query">
+							<input class="js-typeahead" name="utouch_posts[query]" placeholder="What are you looking for?" autocomplete="off" type="search" autofocus>
+						</span>
+                        <button type="submit" class="form-icon">
+                            <svg class="utouch-icon utouch-icon-search"><use xlink:href="#utouch-icon-search"></use></svg>
+                        </button>
+                        <span class="close js-popup-clear-input form-icon">
+							<svg class="utouch-icon utouch-icon-cancel-1"><use xlink:href="#utouch-icon-cancel-1"></use></svg>
+						</span>
+
+                    </div>
+                </div>
+            </form>
+        </div>
+
+    </div>
+
+</div>
