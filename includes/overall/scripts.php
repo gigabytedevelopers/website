1d2a22f2de49aa0251050fad5cba92a89b3780d5
Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>
2020-05-23 16:23:12 +0100

    Added live chat support - Drift
    Signed-off-by: Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>

--- includes/overall/scripts.php
+++ includes/overall/scripts.php
@@ -6,3 +6,38 @@
 <script src="js/velocity.min.js"></script>
 <script src="js/main.js"></script>
 <script src="js/form-actions.js"></script>
+<!-- <script type="text/javascript" src="js/maintainscroll.js"></script> -->
+<!-- <script src="//code.jivosite.com/widget/INiXHwF7h4" async></script> -->
+
+<!-- Start of HubSpot Embed Code -->
+  <!-- <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/7618166.js"></script> -->
+<!-- End of HubSpot Embed Code -->
+
+<!-- Start of Async Drift Code -->
+<script>
+"use strict";
+
+!function() {
+  var t = window.driftt = window.drift = window.driftt || [];
+  if (!t.init) {
+    if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
+    t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ],
+    t.factory = function(e) {
+      return function() {
+        var n = Array.prototype.slice.call(arguments);
+        return n.unshift(e), t.push(n), t;
+      };
+    }, t.methods.forEach(function(e) {
+      t[e] = t.factory(e);
+    }), t.load = function(t) {
+      var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
+      o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
+      var i = document.getElementsByTagName("script")[0];
+      i.parentNode.insertBefore(o, i);
+    };
+  }
+}();
+drift.SNIPPET_VERSION = '0.3.1';
+drift.load('remchpx6n9es');
+</script>
+<!-- End of Async Drift Code -->
