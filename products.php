2ad46a5757b7b07ab25cc58af408c3bd3d04dffd
Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>
2020-05-23 14:13:56 +0100

    Started Products page
    Signed-off-by: Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>

---
+++ products.php
@@ -0,0 +1,272 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<title>Gigabyte Developers - Products</title>
+
+    <meta name="google-site-verification" content="RjAgqUgYfT94lQ7oGZqqRI87NeSdR_6fecSUipiGBmg" />
+    <meta name="msvalidate.01" content="F39E6D2A441B34A5BCA816D83B967BDF" />
+    <meta property="og:image:secure_url" content="https://raw.githubusercontent.com/gigabytedevelopers/website/master/thumbnail.png" />
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="Gigabyte Developers Incorporated">
+    <meta property="og:title" content="Gigabyte Developers - Pricing" />
+    <meta property="og:description" content="Products Portfolio of Gigabyte Developers" />
+    <meta property="og:image" itemprop="image" content="https://raw.githubusercontent.com/gigabytedevelopers/website/master/thumbnail.png" />
+    <meta property="og:image:url" itemprop="image" content="https://raw.githubusercontent.com/gigabytedevelopers/website/master/thumbnail.png" />
+    <meta property="og:image:type" content="image/png" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Products Portfolio of Gigabyte Developers">
+    <meta name="author" content="Gigabyte Developers Incorporated">
+    <meta name="theme-color" content="#028fcc">
+    <link rel="shortcut icon" href="img/gigaicon.png">
+
+	<link rel="stylesheet" type="text/css" href="css/theme-styles.css">
+	<link rel="stylesheet" type="text/css" href="css/blocks.css">
+	<link rel="stylesheet" type="text/css" href="css/widgets.css">
+
+
+	<!--External fonts-->
+	<link href="https://fonts.googleapis.com/css?family=Nunito:300,400,700,900" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Baloo+Paaji" rel="stylesheet">
+
+	<!-- Styles for Plugins -->
+	<link rel="stylesheet" type="text/css" href="css/swiper.min.css">
+
+	<!--Styles for RTL-->
+
+	<!--<link rel="stylesheet" type="text/css" href="css/rtl.css">-->
+
+
+</head>
+<body class="crumina-grid">
+<?php include_once("includes/overall/googleanalyticstracking.php") ?>
+
+<!-- Preloader -->
+<div id="hellopreloader" style="display: block; position: fixed;z-index: 99999;top: 0;left: 0;width: 100%;height: 100%;min-width: 100%;background: #66b5ff url(svg/preload.svg) center center no-repeat;background-size: 41px;opacity: 1;"></div>
+<!-- ... end Preloader -->
+
+<!-- Header -->
+<?php include 'includes/overall/header/white/header.php'; ?>
+<!-- ... End Header -->
+
+
+<div class="content-wrapper">
+
+
+	<section class="background-contain bg-17 pt100">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-12 col-sm-offset-0">
+					<div class="crumina-module crumina-heading align-center">
+						<h6 class="heading-sup-title">Our Products</h6>
+						<h2 class="heading-title"><span class="c-primary">Gigabyte Developers</span> is an Inspiring Tech leadership innovation Company</h2>
+					</div>
+				</div>
+				<div class="col-lg-12 col-sm-12">
+					<img src="img/product-img.png" alt="image" class="display-block">
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+	<!-- Zoom Image -->
+
+	<section class="bg-primary-color medium-padding120">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+					<div class="crumina-module crumina-zoom-image">
+						<img src="img/smartphone_back.png" alt="smartphone">
+						<div class="zoom-round-img">
+							<img src="img/ouros.png" alt="zoom">
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+					<div class="crumina-module crumina-heading custom-color c-white">
+						<h2 class="heading-title">Cutting Edge App Designs</h2>
+						<div class="heading-text">We create beautiful, interactive Mobile app solutions. For Android, we integrate the latest material design specifications for Android. For iOS devices, we make sure that all our software solutions meet the Flat design specification for iOS. A list of our most featured mobile applications for Android are:
+						</div>
+					</div>
+					<ul class="list list--breez">
+						<li>
+							<svg class="utouch-icon utouch-icon-checked"><use xlink:href="#utouch-icon-checked"></use></svg>
+							<a href="#">
+                                Splanner - The #1 Student planner for Android
+                            </a>
+						</li>
+						<li>
+							<svg class="utouch-icon utouch-icon-checked"><use xlink:href="#utouch-icon-checked"></use></svg>
+							<a href="https://play.google.com/store/apps/details?id=com.gigabytedevelopersinc.app.explorer" target="_blank">
+                                FireFiles - All-in-One File Manager for Android
+                            </a>
+						</li>
+						<li>
+							<svg class="utouch-icon utouch-icon-checked"><use xlink:href="#utouch-icon-checked"></use></svg>
+							<a href="https://play.google.com/store/apps/details?id=com.gigabytedevelopersinc.app.CometOTP" target="_blank">
+                                CometOTP - The leading Authenticator for Android
+                            </a>
+						</li>
+						<li>
+							<svg class="utouch-icon utouch-icon-checked"><use xlink:href="#utouch-icon-checked"></use></svg>
+							<a href="https://play.google.com/store/apps/details?id=com.gigabytedevsinc.mezue.ouros" target="_blank">
+                                Ouros - Material design Icon Pack for Android
+                            </a>
+						</li>
+						<li>
+							<svg class="utouch-icon utouch-icon-checked"><use xlink:href="#utouch-icon-checked"></use></svg>
+							<a href="https://play.google.com/store/apps/details?id=com.gigabytedevelopersinc.apps.sonshub" target="_blank">
+                                SonsHub Mobile - Latest Gospel, Music, Videos
+                            </a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- ... end Zoom Image -->
+
+	<!-- Screenshots -->
+
+	<section class="align-center bg-blue-lighteen medium-padding120">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-12 col-xs-12">
+					<div class="crumina-module crumina-heading align-center">
+						<h6 class="heading-sup-title">Screenshots</h6>
+						<h2 class="heading-title">Beautiful Design and Comfortable User Interface</h2>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="crumina-module crumina-screenshots">
+			<div class="col-item">
+				<div>
+					<img src="img/foodie.png" alt="screen">
+					<div class="overlay-standard overlay--blue-dark"></div>
+					<a href="img/foodie.png" class="expand js-zoom-image">
+						<svg class="utouch-icon utouch-icon-expand"><use xlink:href="#utouch-icon-expand"></use></svg>
+					</a>
+				</div>
+			</div>
+			<div class="col-item">
+				<div>
+					<img src="img/calcmatemain.png" alt="screen">
+					<div class="overlay-standard overlay--blue-dark"></div>
+					<a href="img/calcmatemain.png" class="expand js-zoom-image">
+						<svg class="utouch-icon utouch-icon-expand"><use xlink:href="#utouch-icon-expand"></use></svg>
+					</a>
+				</div>
+				<div>
+					<img src="img/umbra.png" alt="screen">
+					<div class="overlay-standard overlay--blue-dark"></div>
+					<a href="img/umbra.png" class="expand js-zoom-image">
+						<svg class="utouch-icon utouch-icon-expand"><use xlink:href="#utouch-icon-expand"></use></svg>
+					</a>
+				</div>
+			</div>
+			<div class="col-item">
+				<div>
+					<img src="img/portfolio4s.png" alt="screen">
+					<div class="overlay-standard overlay--blue-dark"></div>
+					<a href="img/portfolio4s.png" class="expand js-zoom-image">
+						<svg class="utouch-icon utouch-icon-expand"><use xlink:href="#utouch-icon-expand"></use></svg>
+					</a>
+				</div>
+			</div>
+			<div class="col-item">
+				<div>
+					<img src="img/firefiles.png" alt="screen">
+					<div class="overlay-standard overlay--blue-dark"></div>
+					<a href="img/firefiles.png" class="expand js-zoom-image">
+						<svg class="utouch-icon utouch-icon-expand"><use xlink:href="#utouch-icon-expand"></use></svg>
+					</a>
+				</div>
+				<div>
+					<img src="img/splannersetting.png" alt="screen">
+					<div class="overlay-standard overlay--blue-dark"></div>
+					<a href="img/splannersetting.png" class="expand js-zoom-image">
+						<svg class="utouch-icon utouch-icon-expand"><use xlink:href="#utouch-icon-expand"></use></svg>
+					</a>
+				</div>
+			</div>
+			<div class="col-item">
+				<div>
+					<img src="img/readmanga.png" alt="screen">
+					<div class="overlay-standard overlay--blue-dark"></div>
+					<a href="img/readmanga.png" class="expand js-zoom-image">
+						<svg class="utouch-icon utouch-icon-expand"><use xlink:href="#utouch-icon-expand"></use></svg>
+					</a>
+				</div>
+			</div>
+		</div>
+
+	</section>
+
+	<!-- ... end Screenshots -->
+
+
+	<!-- Promo Block -->
+
+	<section class="background-cover bg-18 align-center medium-padding120">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-12 col-sm-offset-0 col-xs-12">
+					<div class="crumina-module crumina-heading">
+						<h2 class="heading-title">Download <span class="c-primary">our app</span> Now</h2>
+						<p class="heading-text">Try out Our mobile app today.
+						</p>
+					</div>
+
+					<a href="#"   class="btn btn-market btn--with-shadow">
+						<svg class="utouch-icon utouch-icon-apple-logotype-1"><use xlink:href="#utouch-icon-apple-logotype-1"></use></svg>
+						<div class="text">
+							<span class="sup-title">Download on the</span>
+							<span class="title">App Store</span>
+                            <span class="title">Coming Soon</span>
+						</div>
+					</a>
+
+					<a href="https://play.google.com/store/apps/dev?id=4617962611760843834"   class="btn btn-market btn--with-shadow">
+						<img class="utouch-icon" src="svg-icons/google-play.svg" alt="google">
+						<div class="text">
+							<span class="sup-title">Download on the</span>
+							<span class="title">Google Play</span>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- ... end Promo Block -->
+</div>
+
+<!-- Footer -->
+<?php include 'includes/overall/footer.php'; ?>
+<!-- End Footer -->
+
+<!-- Send Message Popup -->
+<?php include 'includes/overall/send_message_popup.php'; ?>
+<!-- Send Message Popup -->
+
+<!-- Search Popup -->
+<?php include 'includes/overall/search_popup.php'; ?>
+<!-- ... end Search Popup -->
+
+<!-- Used SVG-icons -->
+<?php include 'includes/overall/svg_icons.php'; ?>
+<!-- ... end Used SVG-icons -->
+
+
+<!-- jQuery first, then Other JS. -->
+<!-- jQuery-scripts -->
+<?php include 'includes/overall/scripts.php'; ?>
+</body>
+</html>
