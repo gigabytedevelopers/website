1c489af90f67bc758d96c2b768820f9bf9bbb243
Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>
2020-05-23 14:47:09 +0100

    Created Terms and Conditions, Privacy Policy page
    Signed-off-by: Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>

---
+++ privacy.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>Gigabyte Developers - Privacy Policy</title>
+
+    <meta name="google-site-verification" content="RjAgqUgYfT94lQ7oGZqqRI87NeSdR_6fecSUipiGBmg" />
+    <meta name="msvalidate.01" content="F39E6D2A441B34A5BCA816D83B967BDF" />
+    <meta property="og:image:secure_url" content="https://raw.githubusercontent.com/gigabytedevelopers/website/master/thumbnail.png" />
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="Gigabyte Developers Incorporated">
+    <meta property="og:title" content="Gigabyte Developers - Pricing" />
+    <meta property="og:description" content="Privacy Policy of Gigabyte Developers" />
+    <meta property="og:image" itemprop="image" content="https://raw.githubusercontent.com/gigabytedevelopers/website/master/thumbnail.png" />
+    <meta property="og:image:url" itemprop="image" content="https://raw.githubusercontent.com/gigabytedevelopers/website/master/thumbnail.png" />
+    <meta property="og:image:type" content="image/png" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Privacy Policy of Gigabyte Developers">
+    <meta name="author" content="Gigabyte Developers Incorporated">
+    <meta name="theme-color" content="#028fcc">
+    <link rel="shortcut icon" href="img/gigaicon.png">
+
+    <link rel="stylesheet" type="text/css" href="css/theme-styles.css">
+    <link rel="stylesheet" type="text/css" href="css/blocks.css">
+    <link rel="stylesheet" type="text/css" href="css/widgets.css">
+
+
+    <!--External fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:300,400,700,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Baloo+Paaji" rel="stylesheet">
+
+    <!-- Styles for Plugins -->
+    <link rel="stylesheet" type="text/css" href="css/swiper.min.css">
+
+    <!--Styles for RTL-->
+
+    <!--<link rel="stylesheet" type="text/css" href="css/rtl.css">-->
+
+
+</head>
+<body class="crumina-grid">
+<?php include_once("includes/overall/googleanalyticstracking.php") ?>
+
+<!-- Preloader -->
+<div id="hellopreloader" style="display: block; position: fixed;z-index: 99999;top: 0;left: 0;width: 100%;height: 100%;min-width: 100%;background: #66b5ff url(svg/preload.svg) center center no-repeat;background-size: 41px;opacity: 1;"></div>
+<!-- ... end Preloader -->
+
+<!-- Header -->
+<?php include 'includes/overall/header/white/header.php'; ?>
+<!-- ... End Header -->
+
+<div class="content-wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>
+                        Our Privacy Policy
+                    </h1>
+                    <p style="text-align: right">
+                        Last updated: <b>January 05, 2018</b>
+                    </p>
+                </div>
+            </div>
+        </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-12 col-sm-offset-0">
+                <p>Gigabyte Developers Incorporated ("us", "we", or "our") operates the <b><i>https://www.gigabytedevelopersinc.com</i></b> website (the "Service").</p>
+
+                <p>This page informs you of our policies regarding the collection, use and disclosure of Personal Information when you use our Service.</p>
+
+                <p>We will not use or share your information with anyone except as described in this Privacy Policy.</p>
+
+                <p>We use your Personal Information for providing and improving the Service. By using the Service, you agree to the collection and use of information in accordance with this policy. Unless otherwise defined in this Privacy Policy, terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, accessible at <a href="https://www.gigabytedevelopersinc.com/terms" style="color: #0083ff;">https://www.gigabytedevelopersinc.com/terms</a></p>
+
+
+                <h2>Information Collection And Use</h2>
+
+                <p>While using our Service, we may ask you to provide us with certain personal identifiable information that can be used to contact or identify you. Personal Identifiable Information may include, but is not limited to, your name, phone number, postal address ("Personal Information").</p>
+
+                <h2>Log Data</h2>
+
+                <p>We collect information that your browser sends whenever you visit our Service ("Log Data"). This Log Data may include information such as your computer's Internet Protocol ("IP") address, browser type, browser version, the pages of our Service that you visit, the time and date of your visit, the time spent on those pages and other statistics.</p>
+
+                <h2>Cookies</h2>
+
+                <p>Cookies are files with small amount of data, which may include an anonymous unique identifier. Cookies are sent to your browser from a web site and stored on your computer's hard drive.</p>
+
+                <p>We use "cookies" to collect information. You can instruct your browser to refuse all cookies or to indicate when a cookie is being sent. However, if you do not accept cookies, you may not be able to use some portions of our Service.</p>
+
+                <h2>Service Providers</h2>
+
+                <p>We may employ third party companies and individuals to facilitate our Service, to provide the Service on our behalf, to perform Service-related services or to assist us in analyzing how our Service is used.</p>
+
+                <p>These third parties have access to your Personal Information only to perform these tasks on our behalf and are obligated not to disclose or use it for any other purpose.</p>
+
+                <h2>Security</h2>
+
+                <p>The security of your Personal Information is important to us, but remember that no method of transmission over the Internet, or method of electronic storage is 100% secure. While we strive to use commercially acceptable means to protect your Personal Information, we cannot guarantee its absolute security.</p>
+
+                <h2>Links To Other Sites</h2>
+
+                <p>Our Service may contain links to other sites that are not operated by us. If you click on a third party link, you will be directed to that third party's site. We strongly advise you to review the Privacy Policy of every site you visit.</p>
+
+                <p>We have no control over, and assume no responsibility for the content, privacy policies or practices of any third party sites or services.</p>
+
+                <h2>Children's Privacy</h2>
+
+                <p>Our Service does not address anyone under the age of 13 ("Children").</p>
+
+                <p>We do not knowingly collect personal identifiable information from children under 13. If you are a parent or guardian and you are aware that your Children has provided us with Personal Information, please contact us. If we discover that a Children under 13 has provided us with Personal Information, we will delete such information from our servers immediately.</p>
+
+                <h2>Changes To This Privacy Policy</h2>
+
+                <p>We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page.</p>
+
+                <p>Thus, you are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are effective immediately after they are posted on this page.</p>
+
+                <h2>Contact Us</h2>
+
+                <p>If you have any questions about this Privacy Policy, please click on the button below to contact us.</p>
+                <div class="col-lg-12 col-md-12 col-sm-12" style="margin-top: -50px">
+                    <div class="align-center">
+                        <a href="contact" class="btn btn-border btn--with-shadow btn-more c-primary"> <!--id="load-more-button" data-load-link="items-to-load.html" data-container="items-grid"-->Contact Us Now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Footer -->
+<?php include 'includes/overall/footer.php'; ?>
+<!-- End Footer -->
+
+<!-- Send Message Popup -->
+<?php include 'includes/overall/send_message_popup.php'; ?>
+<!-- Send Message Popup -->
+
+<!-- Search Popup -->
+<?php include 'includes/overall/search_popup.php'; ?>
+<!-- ... end Search Popup -->
+
+<!-- Used SVG-icons -->
+<?php include 'includes/overall/svg_icons.php'; ?>
+<!-- ... end Used SVG-icons -->
+
+
+<!-- jQuery first, then Other JS. -->
+<!-- jQuery-scripts -->
+<?php include 'includes/overall/scripts.php'; ?>
+</body>
+</html>
---
+++ terms.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>Gigabyte Developers - Terms & Conditions</title>
+
+    <meta name="google-site-verification" content="RjAgqUgYfT94lQ7oGZqqRI87NeSdR_6fecSUipiGBmg" />
+    <meta name="msvalidate.01" content="F39E6D2A441B34A5BCA816D83B967BDF" />
+    <meta property="og:image:secure_url" content="https://raw.githubusercontent.com/gigabytedevelopers/website/master/thumbnail.png" />
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="Gigabyte Developers Incorporated">
+    <meta property="og:title" content="Gigabyte Developers - Pricing" />
+    <meta property="og:description" content="Terms and Conditions of Gigabyte Developers" />
+    <meta property="og:image" itemprop="image" content="https://raw.githubusercontent.com/gigabytedevelopers/website/master/thumbnail.png" />
+    <meta property="og:image:url" itemprop="image" content="https://raw.githubusercontent.com/gigabytedevelopers/website/master/thumbnail.png" />
+    <meta property="og:image:type" content="image/png" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Terms and Conditions of Gigabyte Developers">
+    <meta name="author" content="Gigabyte Developers Incorporated">
+    <meta name="theme-color" content="#028fcc">
+    <link rel="shortcut icon" href="img/gigaicon.png">
+
+    <link rel="stylesheet" type="text/css" href="css/theme-styles.css">
+    <link rel="stylesheet" type="text/css" href="css/blocks.css">
+    <link rel="stylesheet" type="text/css" href="css/widgets.css">
+
+
+    <!--External fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:300,400,700,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Baloo+Paaji" rel="stylesheet">
+
+    <!-- Styles for Plugins -->
+    <link rel="stylesheet" type="text/css" href="css/swiper.min.css">
+
+    <!--Styles for RTL-->
+
+    <!--<link rel="stylesheet" type="text/css" href="css/rtl.css">-->
+
+
+</head>
+<body class="crumina-grid">
+<?php include_once("includes/overall/googleanalyticstracking.php") ?>
+
+<!-- Preloader -->
+<div id="hellopreloader" style="display: block; position: fixed;z-index: 99999;top: 0;left: 0;width: 100%;height: 100%;min-width: 100%;background: #66b5ff url(svg/preload.svg) center center no-repeat;background-size: 41px;opacity: 1;"></div>
+<!-- ... end Preloader -->
+
+<!-- Header -->
+<?php include 'includes/overall/header/white/header.php'; ?>
+<!-- ... End Header -->
+
+<div class="content-wrapper">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>
+                    Our Terms and Conditions
+                </h1>
+                <p style="text-align: right">
+                    Last updated: <b>January 05, 2018</b>
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-12 col-sm-offset-0">
+                <h2>Introduction</h2>
+
+                <p>These Website Standard Terms and Conditions written on this webpage shall manage your use of our website, <b>Gigabyte Developers</b> accessible at <a href="https://www.gigabytedevelopersinc.com/terms" style="color: #0083ff;">https://www.gigabytedevelopersinc.com/terms</a></p>
+
+                <p>These Terms will be applied fully and affect to your use of this Website. By using this Website, you agreed to accept all terms and conditions written in here. You must not use this Website if you disagree with any of these Website's Standard Terms and Conditions.</a></p>
+
+                <p>Children or people below 13 years old are not allowed to use this Website.</p>
+
+                <h2>Intellectual Property Rights</h2>
+
+                <p>Other than the content you own, under these Terms, Gigabyte Developers and/or its licensors own all the intellectual property rights and materials contained in this Website.</p>
+
+                <p>You are granted limited license only for purposes of viewing the material contained on this Website.</p>
+
+                <h2>Restrictions</h2>
+
+                <p>You are specifically restricted from all of the following:</p>
+
+                <ol>
+                    <li>publishing any Website material in any other media;</li>
+                    <li>selling, sublicensing and/or otherwise commercializing any Website material;</li>
+                    <li>publicly performing and/or showing any Website material;</li>
+                    <li>using this Website in any way that is or may be damaging to this Website;</li>
+                    <li>using this Website in any way that impacts user access to this Website;</li>
+                    <li>using this Website contrary to applicable laws and regulations, or in any way may cause harm to the Website, or to any person or business entity;</li>
+                    <li>engaging in any data mining, data harvesting, data extracting or any other similar activity in relation to this Website;</li>
+                    <li>using this Website to engage in any advertising or marketing.</li>
+                </ol>
+
+                <p>Certain areas of this Website are restricted from being accessed by you and Gigabyte Developers may further restrict access by you to any areas of this Website, at any time, in absolute discretion. Any user ID and password you may have for this Website are confidential and you must maintain confidentiality as well.</p>
+
+                <h2>Your Content</h2>
+
+                <p>In these Website Standard Terms and Conditions, "Your Content" shall mean any audio, video text, images or other material you choose to display on this Website. By displaying Your Content, you grant Gigabyte Developers a non-exclusive, worldwide irrevocable, sub licensable license to use, reproduce, adapt, publish, translate and distribute it in any and all media.</p>
+
+                <p>Your Content must be your own and must not be invading any third-party’s rights. Gigabyte Developers reserves the right to remove any of Your Content from this Website at any time without notice.</p>
+
+                <h2>No warranties</h2>
+
+                <p>This Website is provided "as is," with all faults, and Gigabyte Developers express no representations or warranties, of any kind related to this Website or the materials contained on this Website. Also, nothing contained on this Website shall be interpreted as advising you.</p>
+
+                <h2>Limitation of liability</h2>
+
+                <p>In no event shall Gigabyte Developers, nor any of its officers, directors and employees, be held liable for anything arising out of or in any way connected with your use of this Website whether such liability is under contract.  Gigabyte Developers, including its officers, directors and employees shall not be held liable for any indirect, consequential or special liability arising out of or in any way related to your use of this Website.</p>
+
+                <h2>Indemnification</h2>
+
+                <p>You hereby indemnify to the fullest extent Gigabyte Developers from and against any and/or all liabilities, costs, demands, causes of action, damages and expenses arising in any way related to your breach of any of the provisions of these Terms.</p>
+
+                <h2>Severability</h2>
+
+                <p>If any provision of these Terms is found to be invalid under any applicable law, such provisions shall be deleted without affecting the remaining provisions herein.</p>
+
+                <h2>Variation of Terms</h2>
+
+                <p>Gigabyte Developers is permitted to revise these Terms at any time as it sees fit, and by using this Website you are expected to review these Terms on a regular basis.</p>
+
+                <h2>Assignment</h2>
+
+                <p>The Gigabyte Developers is allowed to assign, transfer, and subcontract its rights and/or obligations under these Terms without any notification. However, you are not allowed to assign, transfer, or subcontract any of your rights and/or obligations under these Terms.</p>
+
+                <h2>Entire Agreement</h2>
+
+                <p>These Terms constitute the entire agreement between Gigabyte Developers and you in relation to your use of this Website, and supersedes all prior agreements and understandings.</p>
+
+                <h2>Governing Law & Jurisdiction</h2>
+
+                <p>These Terms will be governed by and interpreted in accordance with the laws of the Federal Republic of Nigeria, and you must submit to the non-exclusive jurisdiction of the State and Federal courts located in Nigeria for the resolution of any disputes.</p>
+
+                <h2>Contact Us</h2>
+
+                <p>If you have any questions about this Terms and Conditions, please click on the button below to contact us.</p>
+                <div class="col-lg-12 col-md-12 col-sm-12" style="margin-top: -50px">
+                    <div class="align-center">
+                        <a href="contact" class="btn btn-border btn--with-shadow btn-more c-primary"> <!--id="load-more-button" data-load-link="items-to-load.html" data-container="items-grid"-->Contact Us Now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Footer -->
+<?php include 'includes/overall/footer.php'; ?>
+<!-- End Footer -->
+
+<!-- Send Message Popup -->
+<?php include 'includes/overall/send_message_popup.php'; ?>
+<!-- Send Message Popup -->
+
+<!-- Search Popup -->
+<?php include 'includes/overall/search_popup.php'; ?>
+<!-- ... end Search Popup -->
+
+<!-- Used SVG-icons -->
+<?php include 'includes/overall/svg_icons.php'; ?>
+<!-- ... end Used SVG-icons -->
+
+
+<!-- jQuery first, then Other JS. -->
+<!-- jQuery-scripts -->
+<?php include 'includes/overall/scripts.php'; ?>
+</body>
+</html>
