ed0285e8655e0181f026c46a7b9e6432ea27cf0a
Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>
2020-05-23 17:44:26 +0100

    Update Contact Page
    Signed-off-by: Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>

--- contact.php
+++ contact.php
@@ -65,34 +65,6 @@
 				<div id="map" style="height: 820px">
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7942.731411890625!2d7.491267931990267!3d5.512613535618069!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1042c333100c0991%3A0xfe612f5a335d5101!2sGigabyte+Developers+Incorporated!5e0!3m2!1sen!2sng!4v1493077965966" width="100%" height="820" frameborder="0" style="border:0" allowfullscreen></iframe>
                 </div>
-                <!--<script>
-                    // Initialize and add the map
-                    function initMap() {
-                      // The location of Gigabyte Developers
-                      var address = {lat: 5.512608, lng: 7.495645};
-                      // The map, centered at Gigabyte Developers
-                      var map = new google.maps.Map(
-                          document.getElementById('map'), {zoom: 17, center: address, scrollwheel: false});
-                      // The marker, positioned at Gigabyte Developers
-                      var marker = new google.maps.Marker({position: address, map: map});
-                    }
-                </script>-->
-				<!--<script>
-					var map;
-					function initMap() {
-                        var address = {lat: 5.512608, lng: 7.495645};
-						map = new google.maps.Map(document.getElementById('map'), {
-							center: {lat: 5.512608, lng: 7.495645},
-							zoom: 12,
-							scrollwheel: false//set to true to enable mouse scrolling while inside the map area
-						});
-					}
-				</script>-->
-				<!--<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAkKEZ-jxBp51pa--wWnoncueVC_6cqtFc&callback=initMap"
-						async defer>
-				</script>-->
-
-				<!-- End Google map -->
 			</div>
 
 			<div class="col-lg-3 col-lg-offset-1 col-md-12 col-sm-12 pt100 pb100">
