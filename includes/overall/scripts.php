6bb23a486afa1a526043321b0cd66850a129132e
Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>
2020-05-23 16:22:17 +0100

    Link JS scripts to site content
    Signed-off-by: Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>

---
+++ includes/overall/scripts.php
@@ -0,0 +1,8 @@
+<script src="js/jquery-3.2.0.min.js"></script>
+<script src="js/crum-mega-menu.js"></script>
+<script src="js/swiper.jquery.min.js"></script>
+<script src="js/theme-plugins.js"></script>
+<script src="js/jquery.typeahead.min.js"></script>
+<script src="js/velocity.min.js"></script>
+<script src="js/main.js"></script>
+<script src="js/form-actions.js"></script>
